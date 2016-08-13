f4384596aecc056551f6c0fe8e25fcfe4bfff928
Farhan Ar Rafi <farhanarrafi@gmail.com>
2016-08-14 05:09:22 +0600

    Updated Landing Page

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -1,11 +1,95 @@
 @extends('landing.layout')
 
 
+@section('header')
+    <div id="header">
+        <div id="header_content">
+            <div id="header_logo"></div>
+            <div id="header_text">
+                <div id="header_text2"><span>“ মনুষ্যত্বের শিক্ষাটাই চরম শিক্ষা আর সমস্তই তার অধীন ”</span></div>
+                <div id="header_text3"><span>-রবীন্দ্রনাথ ঠাকুর</span></div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('menu')
+    <div id="menu">
+        <div id='cssmenu'>
+            <ul>
+                <li><a href='#'><span>বাংলা বিভাগ</span></a></li>
+                <li class='active has-sub'><a href='#'><span>প্রোগ্রাম সমূহ</span></a>
+                    <ul>
+                        <li class='has-sub'><a href='#'><span>বাংলায় অনার্স</span></a>
+                            <ul>
+                                <li><a href='#'><span>XXX</span></a></li>
+                                <li class='last'><a href='#'><span>XXX</span></a></li>
+                            </ul>
+                        </li>
+                        <li class='has-sub'><a href='#'><span>বাংলায় এম,এ</span></a>
+                            <ul>
+                                <li><a href='#'><span>XXX</span></a></li>
+                                <li class='last'><a href='#'><span>XXX</span></a></li>
+                            </ul>
+                        </li>
+                        <li class='has-sub'><a href='#'><span>বাংলায় এম,ফিল</span></a></li>
+                        <li class='has-sub'><a href='#'><span>বাংলায় পি,এইচ,ডি</span></a></li>
+                    </ul>
+                </li>
+                <li><a href='#'><span>শিক্ষকবৃন্দ</span></a></li>
+                <li><a href='#'><span>যোগাযোগ</span></a></li>
+                <li class='last'><a href='#'><span>লগ ইন</span></a></li>
+            </ul>
+        </div>
+    </div>
+@endsection
+
 @section('banner')
     <div id="banner">
-         <script type="text/javascript" src="js/jssor.slider.min.js"></script>
-         <script type="text/javascript" src="js/jssor_1_slider.js"></script>
+        <script type="text/javascript" src="js/jssor.slider.min.js"></script>
+        <script>
+        jssor_1_slider_init = function() {
 
+            var jssor_1_SlideshowTransitions = [
+                {$Duration:1200,$Opacity:2}
+            ];
+
+            var jssor_1_options = {
+                $AutoPlay: true,
+                $SlideshowOptions: {
+                    $Class: $JssorSlideshowRunner$,
+                    $Transitions: jssor_1_SlideshowTransitions,
+                    $TransitionsOrder: 1
+                },
+                $ArrowNavigatorOptions: {
+                    $Class: $JssorArrowNavigator$
+                },
+                $BulletNavigatorOptions: {
+                    $Class: $JssorBulletNavigator$
+                }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, auto);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            //responsive code end
+        };
+        </script>
         <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1280px; height: 499px; overflow: hidden; visibility: hidden;">
             <!-- Loading Screen -->
             <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
@@ -37,77 +121,50 @@
             <span data-u="arrowright" class="jssora12r" style="top:0px;right:0px;width:30px;height:46px;" data-autocenter="2"></span>
         </div>
         <script>
-            jssor_1_slider_init();
+        jssor_1_slider_init();
         </script>
     </div>
-    <div id="content_head"><span>Department of Bangla | বাংলা বিভাগ</span></div>
 @endsection
 
 
-@section('header')
-    <div id='cssmenu'>
-    <ul>
-       <li><a href='#'><span>বাংলা বিভাগ</span></a></li>
-       <li class='active has-sub'><a href='/course'><span>প্রোগ্রাম সমূহ</span></a>
-          <ul>
-             <li class='has-sub'><a href='#'><span>বাংলায় অনার্স</span></a>
-                <ul>
-                   <li><a href='#'><span>XXX</span></a></li>
-                   <li class='last'><a href='#'><span>XXX</span></a></li>
-                </ul>
-             </li>
-             <li class='has-sub'><a href='#'><span>বাংলায় এম,এ</span></a>
-                <ul>
-                   <li><a href='#'><span>XXX</span></a></li>
-                   <li class='last'><a href='#'><span>XXX</span></a></li>
-                </ul>
-             </li>
-             <li class='has-sub'><a href='#'><span>বাংলায় এম,ফিল</span></a></li>
-             <li class='has-sub'><a href='#'><span>বাংলায় পি,এইচ,ডি</span></a></li>
-          </ul>
-       </li>
-       <li><a href='faculty'><span>শিক্ষকবৃন্দ</span></a></li>
-       <li><a href='#'><span>যোগাযোগ</span></a></li>
-       @if (Auth::guest())
-           <li><a href="{{ url('/login') }}">লগ ইন</a></li>
-       @else
-           <li class="dropdown">
-               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                   {{ Auth::user()->name }} <span class="caret"></span>
-               </a>
+@section('content_left')
+    <div id="content_left">
+        <span><p>ঐতিহাসিক চিত্রের সূচনা লিখিবার জন্য সম্পাদক-দত্ত অধিকার পাইয়াছি, আর কোনো প্রকারের অধিকারের দাবি রাখি না। কিন্তু আমাদের দেশের সম্পাদক ও পাঠকবর্গ লেখকগণকে যেরূপ প্রচুর পরিমাণে প্রশ্রয় দিয়া থাকেন তাহাতে অনধিকার প্রবেশকে আর অপরাধ বলিয়া জ্ঞান হয় না।
 
-               <ul class="dropdown-menu" role="menu">
-                   <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-               </ul>
-           </li>
-       @endif
-    </ul>
-    </div>
-@endsection
+            এই ঐতিহাসিক পত্রে আমি যদি কিছু লিখিতে সাহস করি তবে তাহা সংক্ষিপ্ত সূচনাটুকু। কোনো শুভ অনুষ্ঠানের উৎসব-উপলক্ষে ঢাকীকে মন্ত্রও পড়িতে হয় না, পরিবেশনও করিতে হয় না– সিংহদ্বারের বাহিরে দাঁড়াইয়া সে কেবল আনন্দধ্বনি ঘোষণা করিতে থাকে। সে যদিচ কর্তাব্যক্তিদের মধ্যে কেহই নহে, কিন্তু সর্বাগ্রে উচ্চকলরবে কার্যারম্ভের সূচনা তাহারই হস্তে।
 
+            যাঁহারা কর্মকর্তা, গীতা তাঁহাদিগকে উপদেশ দিয়াছেন যে: কর্মণ্যেবাধিকারস্তে মা ফলেষু কদাচন। অর্থাৎ, কর্মেই তোমার অধিকার আছে, ফলে কদাচ নাই। আমরা কর্মকর্তা নহি। আমাদের একটা সুবিধা এই যে, কর্মে আমাদের অধিকার নাই, কিন্তু ফলে আছে! সম্পাদক-মহাশয় যে অনুষ্ঠান ও যেরূপ আয়োজন করিয়াছেন তাহার ফল বাংলার, এবং আশা করি অন্য দেশের, পাঠকমণ্ডলী চিরকাল ভোগ করিতে পারিবেন।
 
-@section('facultyPage')
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" height="140" width="140">
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            অদ্য “ঐতিহাসিক চিত্রে’র শুভ জন্মদিনে আমরা যে আনন্দ করিতে উদ্যত হইয়াছি তাহা কেবলমাত্র সাহিত্যের উন্নতিসাধনের আশ্বাসে নহে। তাহার আর-এ</p></span>
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" height="140" width="140">
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" height="140" width="140">
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
-    </div> <!-- /container -->
-@endsection
+    @endsection
+
+    @section('content_right')
+        <div id="content_right">
+            <div id="content_right_header">
+                <div id="content_right_head_pin"></div>
+                <div id="content_right_notice">Notice Board</div>
+            </div>
+            <div id="content_right_content">
+                <div class="textSlide">
+                    <p>বিষয়ঃ বেসরকারী বিশ্ববিদ্যালয় আইন অনুযায়ী প্রয়োজনীয় ব্যবস্থা গ্রহণ প্রসঙ্গে। <br/>
+                        <br/>জরুরী প্রজ্ঞাপন : ১লা আগষ্ট ২০১৬ যথাযথ মর্যাদার সাথে গুলশান ও শোলাকিয়ায় হত্যাকান্ডে ...<br />
+                        <br/>বিষয়ঃ বেসরকারী বিশ্ববিদ্যালয় আইন অনুযায়ী প্রয়োজনীয় ব্যবস্থা গ্রহণ প্রসঙ্গে। <br/>
+                        <br/>জরুরী প্রজ্ঞাপন : ১লা আগষ্ট ২০১৬ যথাযথ মর্যাদার সাথে গুলশান ও শোলাকিয়ায় হত্যাকান্ডে ...<br />
+                        <br/>বিষয়ঃ বেসরকারী বিশ্ববিদ্যালয় আইন অনুযায়ী প্রয়োজনীয় ব্যবস্থা গ্রহণ প্রসঙ্গে। <br/>
+                        <br/>জরুরী প্রজ্ঞাপন : ১লা আগষ্ট ২০১৬ যথাযথ মর্যাদার সাথে গুলশান ও শোলাকিয়ায় হত্যাকান্ডে ...<br />
+                        <br/>বিষয়ঃ বেসরকারী বিশ্ববিদ্যালয় আইন অনুযায়ী প্রয়োজনীয় ব্যবস্থা গ্রহণ প্রসঙ্গে। <br/>
+                        <br/>জরুরী প্রজ্ঞাপন : ১লা আগষ্ট ২০১৬ যথাযথ মর্যাদার সাথে গুলশান ও শোলাকিয়ায় হত্যাকান্ডে ...</p>
+                    </div>
+                </div>
+            </div>
+        @endsection
+
+        @section('footer')
+            <div id="footer">
+                <div id="footer_content">
+                    <div id="footer_text_left"><span>Jahangirnagar University, Savar, Dhaka-1342, Bangladesh.<br />Telephone: PABX: 880-2-7791045-51, 			Fax: 880-2-7791052</span></div>
+                    <div id="footer_text_right"><span>&copy; 2016 Jahangirnagar University</span></div>
+                </div>
+            </div>
+        @endsection
--- resources/views/landing/layout.blade.php
+++ resources/views/landing/layout.blade.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
-<html>
-  <head>
+<html lang="bn">
+<head>
     <meta charset="utf-8">
     <title>Department of Bangla</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/css/style.css" rel="stylesheet" type="text/css">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="main_content">
-		<div class="header"></div>
-
-            @yield('header')
-
-            @yield('banner')
-
-
-
-        <div id="content">
-
-            @yield('content')
-
+</head>
+<body>
+    <div id="main_content">
+        @yield('header')
+        @yield('menu')
+        @yield('banner')
+        <div id="content_head"><div id="content_head_text"><span>Department of Bangla | বাংলা বিভাগ</span></div></div>
+        <div id="content_div">
+            <div id="content">
+                    @yield('content_left')
+                    @yield('content_right')
+            </div>
+                @yield('footer')
         </div>
-        <div id="footer"><span>বাংলা বিভাগ | জাহাঙ্গীরনগর বিশ্ববিদ্যালয়</span></div>
     </div>
     <script>window.jQuery || document.write('<script src="/js/jquery-2.2.4.min.js"><\/script>')</script>
     <script src="/js/bootstrap.min.js"></script>
-  </body>
+</body>
 </html>
