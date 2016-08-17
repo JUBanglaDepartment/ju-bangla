b5493c896c491f31f486d486cf12764c237c7ea3
Farhan Ar Rafi <farhanarrafi@gmail.com>
2016-08-18 00:45:57 +0600

    Updated teahers profile page. Added details parts of home part

--- resources/views/faculty/profile.blade.php
+++ resources/views/faculty/profile.blade.php
@@ -1,6 +1,7 @@
 @extends('landing.layout')
 
 @section('css')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="/css/style.css" rel="stylesheet" type="text/css">
     <link href="/css/sidebar.css" rel="stylesheet">
 @endsection
@@ -19,153 +20,78 @@
     <div id="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm-4 col-md-3 sidebar">
+                <div class="col-sm-2 col-md-3 sidebar">
                     <ul class="nav nav-sidebar">
                         <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Research</a></li>
-                        <li><a href="#">Notice</a></li>
+                        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i>  Home</a></li>
+                        <li><a href="#"><i class="fa fa-book" aria-hidden="true"></i>  Research</a></li>
+                        <li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i>  Notice</a></li>
                     </ul>
                     <ul class="nav nav-sidebar">
-                        <li><a href="">Research Papers</a></li>
-                        <li><a href="">Publications</a></li>
+                        <li><a href=""><i class="fa fa-book" aria-hidden="true"></i>  Research Papers</a></li>
+                        <li><a href=""><i class="fa fa-book" aria-hidden="true"></i>  Publications</a></li>
                     </ul>
                     <ul class="nav nav-sidebar">
-                        <li><a href="">Affiliations</a></li>
-                        <li><a href="">Contact Information</a></li>
+                        <li><a href=""><i class="fa fa-houzz" aria-hidden="true"></i>  Affiliations</a></li>
+                        <li><a href=""><i class="fa fa-phone-square" aria-hidden="true"></i>  Contact Information</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-8 col-md-9">
+                <div class="col-sm-10 col-md-9">
                     <h2 class="page-header">{{$faculty->name}}</h2>
-
-                    <h5 class="sub-header">Email: {{$faculty->email}}</h5>
-                    <h5 class="sub-header">Phone: {{$faculty->phone}}</h5>
-                    <div class="table-responsive">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1,001</td>
-                                    <td>Lorem</td>
-                                    <td>ipsum</td>
-                                    <td>dolor</td>
-                                    <td>sit</td>
-                                </tr>
-                                <tr>
-                                    <td>1,002</td>
-                                    <td>amet</td>
-                                    <td>consectetur</td>
-                                    <td>adipiscing</td>
-                                    <td>elit</td>
-                                </tr>
-                                <tr>
-                                    <td>1,003</td>
-                                    <td>Integer</td>
-                                    <td>nec</td>
-                                    <td>odio</td>
-                                    <td>Praesent</td>
-                                </tr>
-                                <tr>
-                                    <td>1,003</td>
-                                    <td>libero</td>
-                                    <td>Sed</td>
-                                    <td>cursus</td>
-                                    <td>ante</td>
-                                </tr>
-                                <tr>
-                                    <td>1,004</td>
-                                    <td>dapibus</td>
-                                    <td>diam</td>
-                                    <td>Sed</td>
-                                    <td>nisi</td>
-                                </tr>
-                                <tr>
-                                    <td>1,005</td>
-                                    <td>Nulla</td>
-                                    <td>quis</td>
-                                    <td>sem</td>
-                                    <td>at</td>
-                                </tr>
-                                <tr>
-                                    <td>1,006</td>
-                                    <td>nibh</td>
-                                    <td>elementum</td>
-                                    <td>imperdiet</td>
-                                    <td>Duis</td>
-                                </tr>
-                                <tr>
-                                    <td>1,007</td>
-                                    <td>sagittis</td>
-                                    <td>ipsum</td>
-                                    <td>Praesent</td>
-                                    <td>mauris</td>
-                                </tr>
-                                <tr>
-                                    <td>1,008</td>
-                                    <td>Fusce</td>
-                                    <td>nec</td>
-                                    <td>tellus</td>
-                                    <td>sed</td>
-                                </tr>
-                                <tr>
-                                    <td>1,009</td>
-                                    <td>augue</td>
-                                    <td>semper</td>
-                                    <td>porta</td>
-                                    <td>Mauris</td>
-                                </tr>
-                                <tr>
-                                    <td>1,010</td>
-                                    <td>massa</td>
-                                    <td>Vestibulum</td>
-                                    <td>lacinia</td>
-                                    <td>arcu</td>
-                                </tr>
-                                <tr>
-                                    <td>1,011</td>
-                                    <td>eget</td>
-                                    <td>nulla</td>
-                                    <td>Class</td>
-                                    <td>aptent</td>
-                                </tr>
-                                <tr>
-                                    <td>1,012</td>
-                                    <td>taciti</td>
-                                    <td>sociosqu</td>
-                                    <td>ad</td>
-                                    <td>litora</td>
-                                </tr>
-                                <tr>
-                                    <td>1,013</td>
-                                    <td>torquent</td>
-                                    <td>per</td>
-                                    <td>conubia</td>
-                                    <td>nostra</td>
-                                </tr>
-                                <tr>
-                                    <td>1,014</td>
-                                    <td>per</td>
-                                    <td>inceptos</td>
-                                    <td>himenaeos</td>
-                                    <td>Curabitur</td>
-                                </tr>
-                                <tr>
-                                    <td>1,015</td>
-                                    <td>sodales</td>
-                                    <td>ligula</td>
-                                    <td>in</td>
-                                    <td>libero</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="/images/200x200.png" alt="Picture of {{$faculty->name}}" class="img-thumbnail"/>
+                        </div>
+                        <div class="col-md-9">
+                            <h5 class="sub-header"><i class="fa fa-envelope"></i>  {{$faculty->email}}</h5>
+                            <h5 class="sub-header"><i class="fa fa-envelope-o"></i>  {{$faculty->email}}</h5>
+                            <h5 class="sub-header"><i class="fa fa-phone"></i>  {{$faculty->phone}}</h5>
+                            <h5 class="sub-header"><i class="fa fa-skype"></i>  {{$faculty->email}}</h5>
+                            <h5 class="sub-header"><i class="fa fa-facebook"></i>  {{$faculty->email}}</h5>
+                            <h5 class="sub-header"><i class="fa fa-twitter"></i>  {{$faculty->phone}}</h5>
+                        </div>
+                    </div>
+                    <h3 class="page-header">Bio</h3>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>
+                                {{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}
+                            </p>
+                        </div>
+                    </div>
+                    <h3 class="page-header">Current Courses</h3>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <ul class="list-unstyled">
+                                <li>Bengali Literature</li>
+                                <li>Theory of Literary Forms</li>
+                                <li>Bengali Literature</li>
+                                <li>Theory of Literary Forms</li>
+                                <li>Bengali Literature</li>
+                                <li>Theory of Literary Forms</li>
+                                <li>Bengali Literature</li>
+                                <li>Theory of Literary Forms</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h3 class="page-header">Publications</h3>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <ul class="list-unstyled" >
+                                <li>
+                                    <h4>Bengali Literature</h4>
+                                    <p>
+                                        {{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}
+                                    </p>
+                                </li>
+                                <li>
+                                    <h4>Theory of Literary Forms</h4>
+                                    <p>
+                                        {{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}{{$faculty->name}}
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
