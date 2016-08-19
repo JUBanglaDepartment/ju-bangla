e6de89fb2a699e06cc5affd3f95b4655d37a7ab7
Farhan Ar Rafi <farhanarrafi@gmail.com>
2016-08-20 01:42:59 +0600

    Removed Glyphicons

--- resources/views/landing/subview/menu.blade.php
+++ resources/views/landing/subview/menu.blade.php
@@ -1,7 +1,7 @@
 <div id="menu">
     <div id='cssmenu'>
         <ul>
-            <li><a href='#'><span>বাংলা বিভাগ</span></a></li>
+            <li><a href='/'><span>বাংলা বিভাগ</span></a></li>
             <li class='active has-sub'><a href='#'><span>প্রোগ্রাম সমূহ</span></a>
                 <ul>
                     <li class='has-sub'><a href='#'><span>বাংলায় অনার্স</span></a>
@@ -21,7 +21,7 @@
                 </ul>
             </li>
             <li><a href='/faculty'><span>শিক্ষকবৃন্দ</span></a></li>
-            <li><a href='#'><span>যোগাযোগ</span></a></li>
+            <li><a href='/contact'><span>যোগাযোগ</span></a></li>
             <li class='last'><a href='#'><span>লগ ইন</span></a></li>
         </ul>
     </div>
